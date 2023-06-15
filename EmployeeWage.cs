531a017c9f957a4e570961b359754313beb5c5b0
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-15 09:12:42 +0530

    [Shivam Tripathi]: Added Employee Wage

---
+++ EmployeeWage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empwage
+{
+    public class EmployeeWage
+    {
+        public static void EmployeeAttendence()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int result = random.Next(2);
+
+            if (result == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
