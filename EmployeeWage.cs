c427d4d2ed279291a8c17866b030e0d2b4cdda11
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-16 07:57:41 +0530

    [shivam tripathi]: added employee wage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,22 +8,42 @@ namespace empwage
 {
     public class EmployeeWage
     {
+
+        
         public static void EmployeeAttendence()
         {
+             
             int IS_FULL_TIME = 1;
+            int empHrs =0;
+            int empWage = 0;
+
+            int Wage_Per_Hour = 20;
+
+
             Random random = new Random();
 
             int result = random.Next(2);
 
             if (result == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
+                 
             }
 
             else
             {
                 Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+
+            empWage = (empHrs * Wage_Per_Hour);
+
+            Console.WriteLine("Emp Wage : " empWage);
+
+                
+
+
+
         }
     }
 }
